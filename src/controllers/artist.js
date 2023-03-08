f54c0e87ddb47ba40c98933e7517b7923d4b6344
Mario Cooper <mariocooper00@hotmail.com>
2023-03-08 17:40:35 +0000

    creating artists database added

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,5 +1,14 @@
-const createArtist = (req, res) => {
-    res.sendStatus(201);
-};
+const db = require('../db/index.js');
+
+const createArtist = async (req, res) => {
+    const { name, genre } = req.body
+
+    try {
+        const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+        res.status(201).json(artist)
+      } catch (err) {
+        res.status(500).json(err.message)
+      }
+    };
 
 module.exports = { createArtist };
